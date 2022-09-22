aeb253d9b0442cf66abe9ee43f9f402894abd630
Raushan <69568898+u-raushan@users.noreply.github.com>
2022-09-22 23:10:53 +0600

    Update main.m

--- main.m
+++ main.m
@@ -1,2 +1,8 @@
-% Load the data
+% To load the data
 load('ex3data1.mat');
+
+# To display 100 random data points 
+m = size(X, 1);
+rand_indices = randperm(m);
+sel = X(rand_indices(1:100), :);
+displayData(sel);
