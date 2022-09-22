932a328706a0e265100a98f57cd76c0545b17ab5
Raushan <69568898+u-raushan@users.noreply.github.com>
2022-09-23 02:21:12 +0600

    Create lr_predict.m

---
+++ lr_predict.m
@@ -0,0 +1,14 @@
+function p = lr_predict(all_theta, X)
+%PREDICT predicts the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1).
+
+m = size(X, 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+z=X*all_theta';       
+h=sigmoid(z);
+[~,p]=max(h,[],2);
+
+end
