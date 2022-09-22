e648de8ad3189d45581856a7d7d3f6305d09bc45
Raushan <69568898+u-raushan@users.noreply.github.com>
2022-09-23 02:07:57 +0600

    Update main.m

--- main.m
+++ main.m
@@ -6,3 +6,9 @@ m = size(X, 1);
 rand_indices = randperm(m);
 sel = X(rand_indices(1:100), :);
 displayData(sel);
+
+% To train the model for all classes at once 
+num_labels = 10; % 10 labels, from 1 to 10 
+lambda = 0.1;
+[all_theta] = log_regr_oneVsAll(X, y, num_labels, lambda);
+
